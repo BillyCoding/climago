0f621b3257ad5a48e092890ad59421ad2f37c353
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2024-10-18 19:29:23 -0300

    feat: install inter font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home.dart'; // Importando a página inicial
 
 void main() {
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme(
+          // Definindo Inter como a fonte padrão
+          Theme.of(context).textTheme,
+        ),
       ),
       home: Home(),
     );
